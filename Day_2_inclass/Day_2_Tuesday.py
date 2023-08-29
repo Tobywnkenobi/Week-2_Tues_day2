561a6c70f6fdf3bd840357c36281aad8d9052b4b
Tobywnkenobi <141949401+Tobywnkenobi@users.noreply.github.com>
2023-08-29 14:16:51 -0400

    lunchtime commit

---
+++ Day_2_inclass/Day_2_Tuesday.py
@@ -0,0 +1,16 @@
+# students = ['lyla', 'anthony', 'tom', 'heather', 'milad']
+# students2 = students
+# students_copy = students[:]
+
+# students_copy is students
+# students2 is students
+
+# for i, student in enumerate(students_copy):
+#     students_copy[i] - student.title()
+    
+# print(students_copy, '\n', students)
+
+# for i in range(len(students2)):
+#     students2[i] = students2[i].title()
+    
+# print(students2, '\n', students)
---
+++ Day_2_inclass/string_exercies.py
@@ -0,0 +1,15 @@
+# names = ['    coNNor', 'max', ' EVan ', 'JORDAN']
+# HINT: You will need to use a for loop for iteration
+
+names = ['    coNNor', 'max', ' EVan ', 'JORDAN']
+# HINT: You will need to use a for loop for iteration
+final_names = []
+
+for name in names:
+    final_names.append(.strip().title())
+print(final_names)
+
+
+
+
+#strip_names = dunno-lstrip()
